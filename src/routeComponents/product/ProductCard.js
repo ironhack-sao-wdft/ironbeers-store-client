df03bb7f6b43674557593f2fd87a9a60de2cec4a
rschpdr <pedro.resch@gmail.com>
2021-05-12 18:28:14 -0300

    css fixes

--- src/routeComponents/product/ProductCard.js
+++ src/routeComponents/product/ProductCard.js
@@ -12,13 +12,19 @@ function ProductCard(props) {
         className="card card-fixed-height text-dark shadow rounded border-0 m-2"
         style={{ width: "100%" }}
       >
-        <img
-          className="card-img product-img mx-auto mt-2"
-          src={props.beer.image_url}
-          alt="beer"
-        />
+        <div className="card-img-container d-flex justify-content-center align-items-center">
+          <img
+            className="card-img product-img"
+            src={props.beer.image_url}
+            alt="beer"
+          />
+        </div>
+
         <div className="card-body">
-          <h4 className="card-title">
+          <h4
+            title={props.beer.name}
+            className="card-title card-title-fixed-height text-truncate"
+          >
             <small>{props.beer.name}</small>
           </h4>
 
@@ -32,7 +38,7 @@ function ProductCard(props) {
             <small className="card-text">{props.beer.volume}ml</small>
           </p>
 
-          <p className="card-text mb-0">
+          <p className="card-text mb-0 text-truncate">
             <small>{props.beer.tagline}</small>
           </p>
           <p className="card-text mb-0">
